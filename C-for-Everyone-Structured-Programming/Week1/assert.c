24d43ab04a2bc00691a36676f55f36154bcb6703
Bruce <54054499+gengxingri@users.noreply.github.com>
2023-02-17 23:11:52 +0800

    Update assert.c

--- C-for-Everyone-Structured-Programming/Week1/assert.c
+++ C-for-Everyone-Structured-Programming/Week1/assert.c
@@ -1,1 +1,16 @@
+/* Assert.c
+   use assert to find errors within program
+*/
 
+#define NDEBUG // goes before <assert.h>, it turns off the assert
+#include <assert.h>
+#include <stdio.h>
+#include <ctype.h>
+
+
+int main()
+{
+	assert(0);
+	printf("my program runs\n");
+	return 0;
+}
