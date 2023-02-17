63e8f40cd1e95cb1a298023b621f8e7ef0a428c0
Bruce <54054499+gengxingri@users.noreply.github.com>
2023-02-17 12:46:04 +0800

    Add files via upload

---
+++ week1/Print_a_Poem.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void)
+{   
+    printf("Helen,\nthy beauty is to me, \nLike those Nicéan barks of yore,\nThat gently oer a perfumed sea,\nThe weary,\nway-worn wanderer bore, \nTo his own native shore\n");
+    return 0;
+}  
+
+
